cdcc75af5cafaf0342caae08eddafa59e6f9248a
unknown <roman.kora@gmail.com>
2021-05-20 15:19:08 +0200

    Shuffle

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -30,17 +30,21 @@
                 </tr>
             </table>
 
-            <button @click="backToHome()" style="background: green;width: 100px;height: 50px;border: 5px solid red;border-radius: 50px;">naspat</button>
+            <button @click="backToHome()" style="background: green;width: 100px;height: 50px;border: 5px solid red;border-radius: 50px;">zpatki</button>
         </div>
         
     </div>
 
     <div class="game-over" v-if="gameState === 'game-over'">
         <div>
+            <h1 style="color:red">Tadaaaaa</h1>
             <img src="../assets/cat-close.png" alt="" /> <br>
 
-            <button @click="backToHome()" style="background: green;width: 100px;height: 50px;border: 5px solid red;border-radius: 50px;">naspat</button>
+            <button @click="backToHome()" style="background: green;width: 100px;height: 50px;border: 5px solid red;border-radius: 50px;">zpatki</button>
+            <button @click="shuffle()" style="background: green;width: 100px;height: 50px;border: 5px solid red;border-radius: 50px;">znovu</button>
+            
         </div>
+    
     </div> 
   </div>
 
@@ -73,6 +77,7 @@ export default {
   methods: {
     shuffle() {
       this.$store.dispatch('shuffle');
+      this.$store.dispatch('play');
     },
 
     setSize(size) {
